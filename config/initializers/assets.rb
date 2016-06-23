d676a47a2a9d829cd84ef0fb920123c41af600d3
anam33 <andrew.nam@icloud.com>
2016-06-22 20:56:37 -0700

    about and apply draft

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( home.css )
 Rails.application.config.assets.precompile += %w( home.js )
+Rails.application.config.assets.precompile += %w( about.css )
+Rails.application.config.assets.precompile += %w( apply.css )
+Rails.application.config.assets.precompile += %w( clients.css )
