dce999a7bba8e79405687a841ad07c7de37c5c11
anam33 <andrew.nam@icloud.com>
2016-06-30 22:25:15 -0700

    big changes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,10 +9,11 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( turbolinks_transitions.js )
 Rails.application.config.assets.precompile += %w( home.scss )
 Rails.application.config.assets.precompile += %w( about.scss )
 Rails.application.config.assets.precompile += %w( apply.scss )
 Rails.application.config.assets.precompile += %w( clients.scss )
 Rails.application.config.assets.precompile += %w( contact.scss )
 Rails.application.config.assets.precompile += %w( team.scss )
-Rails.application.config.assets.precompile += %w( http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css )
+Rails.application.config.assets.precompile += %w( animate.css )
